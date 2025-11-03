b84a5ded46fb549a2b527847cd97cc4787be8091
shreekala-19 <shreekalapandey@gmail.com>
2025-11-03 11:16:25 +0530

    Day 1 — React project setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# React Arc 160
+This repo contains my 160 days React learning journey✨
---
+++ day1/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "5rem" }}>
+      <h1>Hello World — Day 1 of React Arc 🚀</h1>
+      <p>Learning React step-by-step with Shree!</p>
+    </div>
+  );
+}
+
+export default App;
