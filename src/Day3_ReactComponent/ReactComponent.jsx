df925f57bbe2dd2aa129cfe9bb61d2ae0312519d
shreekala-19 <shreekalapandey@gmail.com>
2025-11-06 11:22:27 +0530

    Added Day 3 - React Component Example

---
+++ src/Day3_ReactComponent/ReactComponent.jsx
@@ -0,0 +1,10 @@
+function ReactComponent() {
+  return (
+    <div>
+      <h1>Day 3: React Component</h1>
+      <p>This is my first custom React component for Day 3 of React ARC.</p>
+    </div>
+  );
+}
+
+export default ReactComponent;
